b77dd24946885bf36fd5d6459903b0dc32c4e67a
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-01-28 03:45:40 +0100

    + alles.jpg

---
+++ logformat.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 3 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
+from sys import argv
+from uuid import uuid4
+import re
+
+class chatlog:
+    def __init__(self, textlog, language):
+        """
+        Input a text/plain chatlog from zweipktfkt and get out HTML5 goodness.
+        """
+
+        self.textlog = textlog
+
+        self.html5log = ""
+
+        firstline = textlog.split("\n")[0]
+
+        self.html5log += '''<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="''' + language + '''">
+<head>
+    <title>Log für ''' + firstline[15:24] + ' ' + firstline[-4:] + '''</title>
+    <link rel="stylesheet" href="css/tango.css" title="Defaul-Stil" type="text/css"/>
+</head>
+<body>'''
+
+        for i, line in enumerate(textlog.split("\n")):
+
+            # replace xml chars
+            line = line.replace("&","&amp;")
+            line = line.replace("<","&lt;")
+            line = line.replace(">","&gt;")
+            line = line.replace("'","&apos;")
+            line = line.replace("\"","&quot;")
+
+            # remove erroneous spaces
+            try:
+                if line[10] == " ":
+                    line = line[:10] + line[11:]
+            except IndexError:
+                pass
+
+            # markup log time, add anchors, mark non-speak lines
+            try:
+                int(line[:2])
+                int(line[3:5])
+                line = '<span class="time">' + line[:5] + '</span>' + line[5:]
+
+                uuid = str(uuid4())
+                if line[32:36] == "&lt;":
+                    line = '<a class="line-marker" href="#' + uuid + '">#</a><span class="line dialog" id="' + uuid + '">' + line + '</span>'
+                else:
+                    line = '<a class="line-marker" href="#' + uuid + '">#</a><span class="line non-dialog" id="' + uuid + '">' + line + '</span>'
+            except:
+                pass
+
+            # markup links
+            uri_pattern = r'''(?<!\()\b([A-Za-z][A-Za-z0-9\+\.\-]*:([A-Za-z0-9\.\-_~:/\?#\[\]@!\$&'\(\)\*\+,;=]|%[A-Fa-f0-9]{2})+)'''
+            uri_replacement = r'''<a href="\1">\1</a>'''
+
+            if re.search(uri_pattern, line):
+                line = re.sub(uri_pattern, uri_replacement, line)
+
+            # mandatory wordfilters
+            line = line.replace("Sascha Lobo", "Dwight D. Eisenhower")
+            line = line.replace("sascha lobo", "dwight d. eisenhower")
+            line = line.replace("saschalobo", "dwightdeisenhower")
+            line = line.replace("Sascha", "Dwight D.")
+            line = line.replace("sascha", "dwight d.")
+            line = line.replace("Lobo", "Eisenhower")
+            line = line.replace("lobo", "eisenhower")
+
+            self.html5log += line + "<br/>\n"
+
+        self.html5log += """</body>
+</html>"""
+
+    def __str__(self):
+        return self.html5log
+
+
+if __name__ == '__main__':
+    infile = argv[1]
+    outfile = infile + ".xhtml"
+
+    f = open(infile, "r")
+    input = f.read()
+    f.close()
+
+    nicelog = str(chatlog(input, "de"))
+
+    g = open(outfile, "w")
+    g.write(nicelog)
+    g.close()
