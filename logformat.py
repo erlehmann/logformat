d12b22982e5286a62a05c6463ba7afabe37f1a9c
Matthias Rampke <matthias@2pktfkt.de>
2010-07-04 15:36:36 +0200

    * output plain text if requested

--- logformat.py
+++ logformat.py
@@ -25,18 +25,17 @@ except:
     pass
 
 class chatlog:
-    def __init__(self, textlog, language):
+    def __init__(self, textlog, language, plain=False):
         """
         Input a text/plain chatlog from zweipktfkt and get out HTML5 goodness.
         """
 
-        self.textlog = textlog
-
-        self.html5log = ""
+        self.log = ""
 
         firstline = textlog.split("\n")[0]
 
-        self.html5log += '''<?xml version="1.0" encoding="UTF-8"?>
+        if not plain:
+            self.log += '''<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="''' + language + '''">
 <head>
@@ -52,17 +51,22 @@ class chatlog:
             line = re.sub(r'(^[0-9]{2}:[0-9]{2} [^<][^ ]*) \(.*@.*\) (has (joined|quit|left))', r'\1 \2', line)
 
             # replace xml chars
-            line = line.replace("&","&amp;")
-            line = line.replace("<","&lt;")
-            line = line.replace(">","&gt;")
-            line = line.replace("'","&apos;")
-            line = line.replace("\"","&quot;")
+            if not plain:
+                line = line.replace("&","&amp;")
+                line = line.replace("<","&lt;")
+                line = line.replace(">","&gt;")
+                line = line.replace("'","&apos;")
+                line = line.replace("\"","&quot;")
 
             # input is mixed utf-8 and latin-1
             try:
-                line = unicode(line,'utf-8','strict').encode('ascii', 'xmlcharrefreplace')
+                line = unicode(line,'utf-8','strict')
             except UnicodeDecodeError:
-                line = unicode(line,'latin-1','strict').encode('ascii', 'xmlcharrefreplace')
+                line = unicode(line,'latin-1','strict')
+            if plain:
+                line = line.encode('utf-8')
+            else:
+                line = line.encode('ascii', 'xmlcharrefreplace')
 
             # remove erroneous spaces
             try:
@@ -71,40 +75,42 @@ class chatlog:
             except IndexError:
                 pass
 
-            # markup log time, add anchors, mark non-speak lines
-            try:
-                int(line[:2])
-                int(line[3:5])
-                line = '<span class="time">' + line[:5] + '</span>' + line[5:]
-    
-                if line[32:36] == "&lt;":
-                    line = '<a class="line-marker" href="#' + str(lineid) + '">#</a><span class="line dialog" id="' + str(lineid) + '">' + line + '</span>'
-                else:
-                    line = '<a class="line-marker" href="#' + str(lineid) + '">#</a><span class="line non-dialog" id="' + str(lineid) + '">' + line + '</span>'
+            if not plain:
+                # markup log time, add anchors, mark non-speak lines
+                try:
+                    int(line[:2])
+                    int(line[3:5])
+                    line = '<span class="time">' + line[:5] + '</span>' + line[5:]
+        
+                    if line[32:36] == "&lt;":
+                        line = '<a class="line-marker" href="#' + str(lineid) + '">#</a><span class="line dialog" id="' + str(lineid) + '">' + line + '</span>'
+                    else:
+                        line = '<a class="line-marker" href="#' + str(lineid) + '">#</a><span class="line non-dialog" id="' + str(lineid) + '">' + line + '</span>'
 
-                lastlineid = lineid
-            except ValueError:
-                pass
+                    lastlineid = lineid
+                except ValueError:
+                    pass
 
-            # markup links
-            uri_patterns = [ r'''((?<=\()\b[A-Za-z][A-Za-z0-9\+\.\-]*:([A-Za-z0-9\.\-_~:/\?#\[\]@!\$&'\(\)\*\+,;=]|%[A-Fa-f0-9]{2})+(?=\)))''', r'''((?<=&lt;)\b[A-Za-z][A-Za-z0-9\+\.\-]*:([A-Za-z0-9\.\-_~:/\?#\[\]@!\$&'\(\)\*\+,;=]|%[A-Fa-f0-9]{2})+(?=&gt;))''', r'''(?<!\()\b([A-Za-z][A-Za-z0-9\+\.\-]*:([A-Za-z0-9\.\-_~:/\?#\[\]@!\$&'\(\)\*\+,;=]|%[A-Fa-f0-9]{2})+)''', ]
-            uri_replacement = r'''<a href="\1">\1</a>'''
+                # markup links
+                uri_patterns = [ r'''((?<=\()\b[A-Za-z][A-Za-z0-9\+\.\-]*:([A-Za-z0-9\.\-_~:/\?#\[\]@!\$&'\(\)\*\+,;=]|%[A-Fa-f0-9]{2})+(?=\)))''', r'''((?<=&lt;)\b[A-Za-z][A-Za-z0-9\+\.\-]*:([A-Za-z0-9\.\-_~:/\?#\[\]@!\$&'\(\)\*\+,;=]|%[A-Fa-f0-9]{2})+(?=&gt;))''', r'''(?<!\()\b([A-Za-z][A-Za-z0-9\+\.\-]*:([A-Za-z0-9\.\-_~:/\?#\[\]@!\$&'\(\)\*\+,;=]|%[A-Fa-f0-9]{2})+)''', ]
+                uri_replacement = r'''<a href="\1">\1</a>'''
 
-            for p in uri_patterns:
-                line, nsubs = re.subn(p, uri_replacement, line)
-                if nsubs > 0: break     # only use first matching pattern
+                for p in uri_patterns:
+                    line, nsubs = re.subn(p, uri_replacement, line)
+                    if nsubs > 0: break     # only use first matching pattern
 
-            self.html5log += line + "<br/>\n"
+            self.log += line + ("\n" if plain else "<br/>\n")
 
-            if line == textlog.split("\n")[-1:][0]:
+            if (not plain) and line == textlog.split("\n")[-1:][0]:
                 link = '''<a class="line-link" href="#''' + str(lastlineid) + '''">⤓</a>'''
-                self.html5log = self.html5log.replace("<!-- link to last line -->", link)
+                self.log = self.log.replace("<!-- link to last line -->", link)
 
-        self.html5log += """</body>
+        if not plain:
+            self.log += """</body>
 </html>"""
 
     def __str__(self):
-        return self.html5log
+        return self.log
 
 
 if __name__ == '__main__':
@@ -122,8 +128,24 @@ if __name__ == '__main__':
     g.close()
 
 def handler(req):
-    req.content_type = "application/xhtml+xml; charset=UTF8"
-    f = open(req.filename)
-    req.write(str(chatlog(f.read(),"de")))
-    f.close()
-    return apache.OK
+    if req.args != None:
+        params = dict([part.split('=') for part in req.args.split('&')])
+        if 'mode' in params and params['mode'] == 'plain':
+            plain = True
+        else:
+            plain = False
+    else:
+        plain = False
+
+    if plain:
+        req.content_type = "text/plain; charset=UTF8"
+        f = open(req.filename)
+        req.write(str(chatlog(f.read(),"de",plain=True)))
+        f.close()
+        return apache.OK
+    else:
+        req.content_type = "application/xhtml+xml; charset=UTF8"
+        f = open(req.filename)
+        req.write(str(chatlog(f.read(),"de",plain=False)))
+        f.close()
+        return apache.OK
